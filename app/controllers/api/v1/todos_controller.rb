d59d3e577116cbb33e9ef127bf32e1e4af8e6a71
David <davidmgurley@gmail.com>
2020-05-21 11:11:51 -0600

    get, put, delete, post

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,45 @@
+module Api
+    module V1
+        class TodosController < ApplicationController
+            def index
+                todos = Todo.order('created_at DESC');
+                render json: {status: 'SUCCESS', message: 'Loaded Todo', data:todos}, status: :ok
+            end
+            def show
+                todo = Todo.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded Todo', data:todo}, status: :ok
+            end
+            def create
+                todo = Todo.new(todo_params)
+                if todo.save
+                    render json: {status: 'SUCCESS', message: 'Saved Todo', data:todo}, status: :ok
+                else
+                    render json: {status: 'Error', message: 'Todo Not Saved', data:todo.errors}, status: :unprocessable_entry
+                end
+
+            end
+
+            def destroy
+                todo = Todo.find(params[:id])
+                todo.destroy
+                render json: {status: 'SUCCESS', message: 'Deleted Todo', data:todo}, status: :ok
+
+            end
+
+            def update
+                todo = Todo.find(params[:id])
+                if todo.update_attributes(todo_params)
+                    render json: {status: 'SUCCESS', message: 'Updated Todo', data:todo}, status: :ok
+                else 
+                    render json: {status: 'Error', message: 'Todo Not Updated', data:todo.errors}, status: :unprocessable_entry
+
+                end
+            end
+
+            private
+            def todo_params
+                params.permit(:title, :description)
+            end
+        end
+    end
+end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ApplicationRecord
-    validates title, presence: true
-    validates description, presence: true
+    validates :title, presence: true
+    validates :description, presence: true
 end
